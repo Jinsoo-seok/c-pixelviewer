d36644047256ec2df75972935b78e79f88dadbe2
bkbkbk2588 <hjk2588@naver.com>
2023-07-05 13:12:18 +0900

    스케줄러 초기 실행 동작 추가

--- src/main/java/com/cudo/pixelviewer/config/scheduler/ScheduleConfig.java
+++ src/main/java/com/cudo/pixelviewer/config/scheduler/ScheduleConfig.java
@@ -12,21 +12,38 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduleConfig {
+    final String FIRST_PREFIX = "firstTime";
     final Scheduler scheduler;
 
     @PostConstruct
     public void run() {
-        JobDetail detail = JobBuilder.newJob(PeriodicJob.class)
-                .withIdentity(PeriodicJob.class.getName())
-                .requestRecovery(true)
-                .build();
-
-        Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity(PeriodicJob.class.getName())
-                .withSchedule(CronScheduleBuilder
-                        .cronSchedule("0 0 0 * * ?"))
-                .build();
         try {
+
+            // 처음 한번 실행
+            JobDetail detailStart = JobBuilder.newJob(PeriodicJob.class)
+                    .withIdentity(FIRST_PREFIX + PeriodicJob.class.getName())
+                    .requestRecovery(true)
+                    .build();
+
+            Trigger triggerStart = TriggerBuilder.newTrigger()
+                    .withIdentity(FIRST_PREFIX + PeriodicJob.class.getName())
+                    .startNow()
+                    .build();
+
+            scheduler.scheduleJob(detailStart, triggerStart);
+
+            // 매일 00:00시에 실행
+            JobDetail detail = JobBuilder.newJob(PeriodicJob.class)
+                    .withIdentity(PeriodicJob.class.getName())
+                    .requestRecovery(true)
+                    .build();
+
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity(PeriodicJob.class.getName())
+                    .withSchedule(CronScheduleBuilder
+                            .cronSchedule("0 0 0 * * ?"))
+                    .build();
+
             scheduler.scheduleJob(detail, trigger);
         } catch (SchedulerException e) {
             log.error(e.toString());
